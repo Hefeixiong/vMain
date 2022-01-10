8d57fc061eb1de1390b6be603a2780e739799c92
hefeixiong <1074535830@qq.com>
2022-01-10 18:18:23 +0800

    完善navbar.vue 样式

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -1,32 +1,39 @@
 <template>
   <div class="navbar rowBC">
     <div class="rowSC">
+      <Hamburger
+          :is-active="true"
+          class="hamburger-container"/>
+    </div>
 
-      <div class="right-menu">
-        <el-dropdown trigger="hover" size="medium">
-          <div class="avatar-wrapper">
-            <img
-                src="https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif?imageView2/1/w/80/h/80"
-                class="user-avatar"
-                alt=""/>
-          </div>
-          <template #dropdown>
-            <el-dropdown-menu>
-              <router-link to="/">
-                <el-dropdown-item>Home</el-dropdown-item>
-              </router-link>
-              <a target="_blank" href="https://github.com/Hefeixiong/vMain">
-                <el-dropdown-item>Github</el-dropdown-item>
-              </a>
-              <a target="_blank" href="https://juejin.cn/post/7036302298435289095">
-                <el-dropdown-item>Docs</el-dropdown-item>
-              </a>
-              <!--<el-dropdown-item>修改密码</el-dropdown-item>-->
-              <el-dropdown-item divided @click="loginOut">login out</el-dropdown-item>
-            </el-dropdown-menu>
-          </template>
-        </el-dropdown>
-      </div>
+    <div  class="heardCenterTitle">xxxx</div>
+
+
+    <div class="right-menu">
+      <el-dropdown trigger="hover" size="medium">
+        <div class="avatar-wrapper">
+          <img
+              src="https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif?imageView2/1/w/80/h/80"
+              class="user-avatar"
+              alt=""/>
+        </div>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <router-link to="/">
+              <el-dropdown-item>Home</el-dropdown-item>
+            </router-link>
+            <a target="_blank" href="https://github.com/Hefeixiong/vMain">
+              <el-dropdown-item>Github</el-dropdown-item>
+            </a>
+            <a target="_blank" href="https://juejin.cn/post/7036302298435289095">
+              <el-dropdown-item>Docs</el-dropdown-item>
+            </a>
+            <!--<el-dropdown-item>修改密码</el-dropdown-item>-->
+            <el-dropdown-item divided @click="loginOut">login out</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+
 
     </div>
 
@@ -34,13 +41,14 @@
 </template>
 
 <script setup lang="ts">
+import Hamburger from './Hanburger';
 
 const loginOut = () => {
   console.log('login out');
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .navbar {
   height: $navBarHeight;
   overflow: hidden;
